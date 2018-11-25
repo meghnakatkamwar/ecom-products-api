516eb655713c2d68c926b9635988578d8c6177b7
Meghna K <katkamwarmeghna@gmail.com>
2018-11-26 00:31:11 +0530

    implemented get, update and delete on product

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { ProductModel } = require('../models')
 
-
+/* Create product */
 router.post('/', function (req, res, next) {
     const { name, description, price } = req.body
     const images = JSON.stringify(req.body.images)
@@ -26,6 +26,44 @@ router.post('/', function (req, res, next) {
 });
 
 
+/* GET product listing. */
+router.get('/', function (req, res, next) {
+    ProductModel.findAll().then((data) => {
+        console.log('product list', data)
+        data = data.map((record) => {
+            record.images = JSON.parse(record.images)
+            return record;
+        })
+        res.send(data)
+    })
+});
 
+/* Update product */
+router.put('/:id', function (req, res, next) {
+    const { name, description, price } = req.body
+    const images = JSON.stringify(req.body.images)
+    ProductModel.findOne({ id: req.params.id }).then((data) => {
+        data.update({
+            name,
+            description,
+            price,
+            images
+        }).then((result) => {
+            console.log('updateResponse', result)
+            res.send(result)
+        })
+    })
+})
+
+/* Delete product */
+router.delete('/:id', function (req, res, next) {
+    ProductModel.destroy({ where: { id: req.params.id } }).then((data) => {
+        console.log('deleted product', data)
+        const result = {
+            status: "success"
+        }
+        res.send(result)
+    })
+})
 
 module.exports = router;
